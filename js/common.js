df21dbc3b1bc7dd7761702b088a63a27a0158aa8
周挺 <wx_faf6f08b64e249d59e7e08887aa94230@git.code.tencent.com>
2020-05-14 10:17:04 +0800

    增加页面

--- js/common.js
+++ js/common.js
@@ -238,13 +238,6 @@ jQuery(function($) {
 });
 
 
-function openwzjc()
-{
-        var url = window.location.href;
-        var title = document.title;
-        window.open("/wzjc/wzjc.htm?wzjcurl=" + url + "&wzjctitle=" + title ,"","height=460,width=500,top=100,left=200,toolbar=no,menubar=no,scrollbars=no,resizable=no,location=no,status=no");
-}
 
 
-
-
+window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"0","bdSize":"24"},"share":{}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];
